421b0798a71ed97a7d80225de244f39835511944
Garcha <rishabhg@amazon.com>
2016-12-27 13:12:59 -0800

    first commit

---
+++ src/main/java/JavaConcurrencyWithoutThePain/ServerExecutorPool.java
@@ -0,0 +1,26 @@
+package JavaConcurrencyWithoutThePain;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by rishabhg on 12/27/16.
+ */
+
+public class ServerExecutorPool {
+    static Executor pool = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(9000);
+        while (true) {
+            final Socket s = socket.accept();
+            pool.execute(() -> {
+                System.out.println("========Thread Name : ========" + Thread.currentThread().getName());
+                System.out.println("========Socket.toString : ========" + s.toString());
+            });
+        }
+    }
+}
---
+++ src/main/java/JavaConcurrencyWithoutThePain/ServerNewThread.java
@@ -0,0 +1,34 @@
+package JavaConcurrencyWithoutThePain;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
+
+public class ServerNewThread {
+
+    private final static Logger LOGGER = Logger.getLogger(ServerNewThread.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        LOGGER.addHandler(new StreamHandler(System.out, new SimpleFormatter()));
+
+        ServerSocket socket = new ServerSocket(9000);
+
+        while (true) {
+            final Socket s = socket.accept();
+
+            new Thread(() -> {
+                doWork(s);
+            }).start();
+        }
+
+    }
+
+    static void doWork(Socket s) {
+        LOGGER.info("========doWork Thread. Socket.toString : ========" + s.toString());
+    }
+
+}
