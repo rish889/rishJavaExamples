a365cd572b2ff6ccb8d44390927011cfb7375d7c
Garcha <rish889@gmail.com>
2017-01-17 16:15:47 -0800

    Callable

--- src/main/java/CoreJava/CompletableFutures/RestClient/RishRestClient.java
+++ src/main/java/CoreJava/CompletableFutures/RestClient/RishRestClient.java
@@ -1,8 +1,12 @@
 package CoreJava.CompletableFutures.RestClient;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -14,10 +18,12 @@ public class RishRestClient {
 
         long start = System.nanoTime();
 
+//        newRunnableCalls();
+        newCallableCalls();
 //        sychronousCalls();
 //        futureCalls();
 //        completatbleFutureCalls();
-        parallelStreamsCall();
+//        parallelStreamsCall();
 
         long duration = (System.nanoTime() - start) / 1_000_000;
         System.out.println("Done in " + duration + " msecs");
@@ -29,42 +35,42 @@ public class RishRestClient {
                 forEach(System.out::println);
     }
 
-    private static void futureCalls() {
+    private static void newRunnableCalls() {
+        LongStream.rangeClosed(1, 32)
+                .mapToObj(l -> new Thread(() -> System.out.println(networkCall(l))))
+                .forEach(l -> l.start());
+    }
 
-        List<Future<String>> list = new ArrayList<>();
-
-        LongStream.rangeClosed(1, 32).
-                mapToObj(l -> executor.submit(() -> networkCall(l))).
-                forEach(f -> {
+    private static void newCallableCalls() {
+        LongStream.rangeClosed(1, 32)
+                .mapToObj(l -> executor.submit(() -> networkCall(l)))
+                .collect(Collectors.toList())
+                .stream()
+                .forEach(l -> {
                     try {
-                        System.out.println(f.get());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
+                        System.out.println(l.get());
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 });
 
+        executor.shutdown();
     }
 
     private static void completatbleFutureCalls() {
-
         LongStream.rangeClosed(1, 32)
                 .mapToObj(l -> CompletableFuture.supplyAsync(() -> networkCall(l), executor))
                 .collect(Collectors.toList())
                 .stream()
                 .map(CompletableFuture::join)
                 .forEach(System.out::println);
-
     }
 
     private static void parallelStreamsCall() {
-
         LongStream.rangeClosed(1, 32)
                 .parallel()
                 .mapToObj(l -> networkCall(l))
                 .forEach(System.out::println);
-
     }
 
     private static String networkCall(long l) {
@@ -74,6 +80,27 @@ public class RishRestClient {
             e.printStackTrace();
         }
         return "Iteration : " + l;
+    }
+
+    private static String realNetworkCall(long l) {
+        StringBuffer response = new StringBuffer();
+        try {
+
+            URL url = new URL("http://localhost:8080/getString/");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response + " : " + l;
     }
 
 }
